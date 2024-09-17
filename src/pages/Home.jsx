4db7526c34bed576cb1836543cb9cb4d84a12c80
BaoBiBo <ng.hoangbao03@gmail.com>
2024-09-17 22:34:59 +0700

    setup router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../layouts/header/header";
+import { Footer } from "../layouts/footer/footer";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>Home</h1>
+      <Footer />
+    </>
+  );
+};
