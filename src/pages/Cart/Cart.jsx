21c4a65d846e4af3f018ea1572f9a1943ddd689c
baobibo <ng.hoangbao03@gmail.com>
2024-10-07 18:15:54 +0700

    Refactor cart page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { getCart, updateCartItem } from "../../services/CartService";
 import { getProdItemById } from "../../services/ProductItemService";
-import "./Cart.css";
 import { Header } from "../../layouts/header/header";
 import { Footer } from "../../layouts/footer/footer";
+
+import "./Cart.css";
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -74,71 +75,77 @@ const Cart = () => {
   return (
     <>
       <Header />
-      <h1 className="text-center">Your Cart</h1>
       <div className="cart-container">
-        {cartItems.length === 0 ? (
-          <p>Your cart is empty.</p>
-        ) : (
-          <>
-            <table className="cart-table">
-              <thead>
-                <tr>
-                  <th className="product-header">Tên Sản Phẩm</th>
-                  <th>Giá</th>
-                  <th>Số Lượng</th>
-                  <th>Tổng</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cartItems.map((item) => (
-                  <tr key={item.productItemId}>
-                    <td className="product-info">
-                      <img
-                        src={item.imageUrl}
-                        alt={item.productName}
-                        className="product-image"
-                      />
-                      <span>{item.productName}</span>
-                    </td>
-                    <td>{item.price.toLocaleString()} VND</td>
-                    <td>
-                      <input
-                        type="number"
-                        min="1"
-                        value={item.quantity}
-                        onChange={(e) =>
-                          handleQuantityChange(
-                            item.productItemId,
-                            parseInt(e.target.value)
-                          )
-                        }
-                        className="quantity-input"
-                      />
-                    </td>
-                    <td>{(item.price * item.quantity).toLocaleString()} VND</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        <main className="cart-content">
+          <h1 className="cart-title">tin tức về cá koi</h1>
+          <div className="cart-grid">
+            {cartItems.length === 0 ? (
+              <p>Your cart is empty.</p>
+            ) : (
+              <>
+                <table className="cart-table">
+                  <thead className="table-header">
+                    <tr>
+                      <th>Tên Sản Phẩm</th>
+                      <th>Giá</th>
+                      <th>Số Lượng</th>
+                      <th>Tổng</th>
+                    </tr>
+                  </thead>
+                  <tbody className="table-body">
+                    {cartItems.map((item) => (
+                      <tr key={item.productItemId}>
+                        <td className="product-info">
+                          <img
+                            src={item.imageUrl}
+                            alt={item.productName}
+                            className="product-image"
+                          />
+                          <span>{item.productName}</span>
+                        </td>
+                        <td>{item.price.toLocaleString()} VND</td>
+                        <td>
+                          <input
+                            type="number"
+                            min="1"
+                            value={item.quantity}
+                            onChange={(e) =>
+                              handleQuantityChange(
+                                item.productItemId,
+                                parseInt(e.target.value)
+                              )
+                            }
+                            className="quantity-input"
+                          />
+                        </td>
+                        <td>
+                          {(item.price * item.quantity).toLocaleString()} VND
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
 
-            <div className="order-summary">
-              <h2>Đặt Hàng</h2>
-              <p>
-                Phí ship: <span>Free ship đơn hàng trên 1.500.000 VND</span>
-              </p>
-              <p>
-                VAT: <span>Không tính phí</span>
-              </p>
-              <h3>Tổng: {calculateTotal()} VND</h3>
-              <div className="order-actions">
-                <button className="checkout-btn" onClick={handleCheckout}>
-                  Tiến Hành Thanh Toán
-                </button>
-                <button className="continue-btn">Tiếp Tục Mua Hàng</button>
-              </div>
-            </div>
-          </>
-        )}
+                <div className="order-summary">
+                  <h2>Đặt Hàng</h2>
+                  <p>
+                    Phí ship: <span>Free ship đơn hàng trên 1.500.000 VND</span>
+                  </p>
+                  <p>
+                    VAT: <span>Không tính phí</span>
+                  </p>
+                  <h3>Tổng: {calculateTotal()} VND</h3>
+                  <div className="order-actions">
+                    <button className="checkout-btn" onClick={handleCheckout}>
+                      Tiến Hành Thanh Toán
+                    </button>
+                    <button className="continue-btn">Tiếp Tục Mua Hàng</button>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        </main>
       </div>
       <Footer />
     </>
