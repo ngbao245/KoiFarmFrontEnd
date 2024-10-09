74830fc3d744d4dbb86130c36e1d82900beace9f
BaoBiBo <ng.hoangbao03@gmail.com>
2024-10-09 08:30:34 +0700

    Fix bug header UI

--- src/layouts/header/AdminHeader.jsx
+++ src/layouts/header/AdminHeader.jsx
@@ -7,7 +7,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
-import "./adminHeader.css"
+import "./adminHeader.css";
 
 const AdminHeader = () => {
   const { logout, user } = useContext(UserContext);
@@ -40,67 +40,71 @@ const AdminHeader = () => {
   }, [dropdownRef]);
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <NavLink className="navbar-brand" to="#">
-          <i
-            className="fa-solid fa-user-tie"
-            style={{ marginRight: "10px" }}
-          ></i>
-          <span> Admin Dashboard</span>
-        </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <>
+      <div className="admin-header-container">
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <NavLink className="navbar-brand" to="#">
+              <i
+                className="fa-solid fa-user-tie"
+                style={{ marginRight: "10px" }}
+              ></i>
+              <span> Admin Dashboard</span>
+            </NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Collapse id="basic-navbar-nav">
-          {((user && user.auth) || window.location.pathname === "/") && (
-            <>
-              <Nav className="me-auto">
-                <NavLink
-                  className="nav-link"
-                  to="/admin"
-                  disabled={!user || !user.auth}
-                >
-                  Staff Management
-                </NavLink>
-                <NavLink
-                  className="nav-link"
-                  to="/admin-product"
-                  disabled={!user || !user.auth}
-                >
-                  Product Item
-                </NavLink>
-              </Nav>
-              <Nav>
-                {user && user.email && (
-                  <span className="nav-link">
-                    Welcome: <span className="fw-bold"> {user.email}</span>
-                  </span>
-                )}
-                <div ref={dropdownRef}>
-                  <NavDropdown
-                    title="Settings"
-                    id="basic-nav-dropdown"
-                    show={showUserDropdown}
-                    onClick={() => setShowUserDropdown((prev) => !prev)}
-                    disabled={!user || !user.auth}
-                  >
-                    {user && user.auth === true ? (
-                      <NavDropdown.Item onClick={handleLogout}>
-                        Logout
-                      </NavDropdown.Item>
-                    ) : (
-                      <NavLink className="dropdown-item" to="/login">
-                        Login
-                      </NavLink>
+            <Navbar.Collapse id="basic-navbar-nav">
+              {((user && user.auth) || window.location.pathname === "/") && (
+                <>
+                  <Nav className="me-auto">
+                    <NavLink
+                      className="nav-link"
+                      to="/admin"
+                      disabled={!user || !user.auth}
+                    >
+                      Staff Management
+                    </NavLink>
+                    <NavLink
+                      className="nav-link"
+                      to="/admin-product"
+                      disabled={!user || !user.auth}
+                    >
+                      Product Item
+                    </NavLink>
+                  </Nav>
+                  <Nav>
+                    {user && user.email && (
+                      <span className="nav-link">
+                        Welcome: <span className="fw-bold"> {user.email}</span>
+                      </span>
                     )}
-                  </NavDropdown>
-                </div>
-              </Nav>
-            </>
-          )}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+                    <div ref={dropdownRef}>
+                      <NavDropdown
+                        title="Settings"
+                        id="basic-nav-dropdown"
+                        show={showUserDropdown}
+                        onClick={() => setShowUserDropdown((prev) => !prev)}
+                        disabled={!user || !user.auth}
+                      >
+                        {user && user.auth === true ? (
+                          <NavDropdown.Item onClick={handleLogout}>
+                            Logout
+                          </NavDropdown.Item>
+                        ) : (
+                          <NavLink className="dropdown-item" to="/login">
+                            Login
+                          </NavLink>
+                        )}
+                      </NavDropdown>
+                    </div>
+                  </Nav>
+                </>
+              )}
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </>
   );
 };
 
