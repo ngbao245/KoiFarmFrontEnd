0835cd74e0965908de129a74a5a4609cd1c0ebc8
BaoBiBo <ng.hoangbao03@gmail.com>
2024-11-15 19:22:42 +0700

    Add batch service

---
+++ src/services/BatchService.js
@@ -0,0 +1,39 @@
+import axios from "./Customize-Axios";
+
+const fetchAllBatchs = () => {
+  return axios.get("Batch/get-all-batches");
+};
+
+const fetchBatchById = (id) => {
+  return axios.get(`Batch/get-batch/${id}`);
+};
+
+const createBatch = (data) => {
+  return axios.post("Batch/create-batch", data);
+};
+
+const updateBatch = (id, data) => {
+  return axios.put(`Batch/update-batch/${id}`, data);
+};
+
+const deleteBatch = (id) => {
+  return axios.delete(`Batch/delete-batch/${id}`);
+};
+
+const addItemToBatch = (id, data) => {
+  return axios.post(`Batch/add-item-to-batch/${id}`, data);
+};
+
+const removeItemFromBatch = (batchId, productItemId) => {
+  return axios.delete(`Batch/delete-batch/${batchId}/${productItemId}`);
+};
+
+export {
+  fetchAllBatchs,
+  fetchBatchById,
+  createBatch,
+  updateBatch,
+  deleteBatch,
+  addItemToBatch,
+  removeItemFromBatch,
+};
